6d79eeb9a6b0388fc4f3071e6550d0fa9ea46921
facjxzdt <facjxzdt@163.com>
2024-02-14 09:14:43 +0800

    fix bugs

--- apps/onekey.py
+++ apps/onekey.py
@@ -2,14 +2,14 @@ import apis.bangumi
 import utils.get_ids, utils.get_score, utils.score, utils.json2csv,utils.sub_anime
 
 print('正在获取新番信息')
-#bgm = apis.bangumi.Bangumi()
-#bgm.get_season_name()
+bgm = apis.bangumi.Bangumi()
+bgm.get_season_name()
 
-#print('正在获取动画id')
-#utils.get_ids.get_ids()
-#print('正在获取动画评分')
-#utils.get_score.get_score(method='air')
-#utils.score.total_score('air')
+print('正在获取动画id')
+utils.get_ids.get_ids()
+print('正在获取动画评分')
+utils.get_score.get_score(method='air')
+utils.score.total_score('air')
 print('存储为csv...')
 utils.json2csv.json2csv(method='air')
 print('完成')
